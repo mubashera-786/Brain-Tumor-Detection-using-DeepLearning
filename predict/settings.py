c12307a21bbfa07ccbb4675e90672af25fce12e3
Mohammed Mubashera <161551512+mubashera-786@users.noreply.github.com>
2025-07-05 15:05:13 +0530

    Update settings.py

--- predict/settings.py
+++ predict/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4i(gvjcha0#(n+rgcfpdo0piw8fd%t)h8ho*h_xcg5o3y1i3f3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
